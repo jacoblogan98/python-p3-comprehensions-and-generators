0913798af5d85a862c4ac114ae4708ae6d0ad83d
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-01-17 21:47:00 -0500

    passed tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # evens = [n for n in num_list if n % 2 == 0]
+    # return evens
+
+    return [n for n in num_list if n % 2 == 0]
+
 
 def make_exclamation(sentence_list):
-    pass
+    # exclamations = [s + "!" for s in sentence_list]
+    # return exclamations
+
+    return [s + "!" for s in sentence_list]
